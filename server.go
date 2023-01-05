588e0290f55802a7da5dc9bc2fb553d616500c78
sayfar <sayfareiei@gmail.com>
2023-01-05 15:05:00 +0700

    Setup Server

--- server.go
+++ server.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/TestAPIsGO", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "TestAPIsGO")
+	})
 	fmt.Println("Please use server.go for main file")
 	fmt.Println("start at port:", os.Getenv("PORT"))
+	log.Fatal(e.Start(os.Getenv("PORT")))
 }
